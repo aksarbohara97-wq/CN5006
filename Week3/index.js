64805e554aedb6f891ff52bdcc7b7b4029e0536c
aksarbohara97-wq <aksarbohara97@gmail.com>
2025-10-17 11:52:13 +0000

    Added weeek3

---
+++ Week3/index.js
@@ -0,0 +1,40 @@
+function EmployeeInfo(name,age)
+{
+    console.log("hii" + name + "your age is" + age)
+    }
+    
+name='Aksar'
+age='20'
+
+EmployeeInfo(name,age)
+
+//code for second exercise
+const EmpSkills=(Skills)=>{
+    console.log("expert in " + Skills)
+}
+EmpSkills("Java")
+
+const student= require('./studentinfo') 
+const person = require('./person')  
+// because getName is the function so we use () 
+console.log("Student Name:" +student.getName())  
+console.log(student.Location())  
+console.log(student.dob)  
+// because dob is a variable so we do nt use () 
+console.log(student.Studentgrade()) 
+console.log("grade is "+student.Studentgrade(55) ) 
+person1= new person("Jim","USA","myemail@gmail.com") 
+console.log("using Person Module",person1.getPersonInfo()) 
+console.log("Programe ended") 
+os=require("os") 
+const util=require('util') 
+console.log("temporary directory"+ os.tmpdir() ) 
+console.log("hostname: "+ os.hostname()) 
+console.log("OS : " + os.platform() +"release:"+ os.release()) 
+console.log("Uptime"+ (os.uptime())/3600 +" hours") 
+console.log("userInfo" + util.inspect(os.userInfo())) 
+console.log("Memory "+ os.totalmem()/1000000000 + "Giga byte") 
+console.log(" free: "+os.freemem()/1000000000 + "Giga byte") 
+console.log("CPU "+ util.inspect(os.cpus())) 
+console.log("Network"+ util.inspect(os.networkInterfaces())) 
+console.log("programe end") 
